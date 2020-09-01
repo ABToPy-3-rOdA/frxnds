ad17744ba76195175c30bd09dac7ea367cc86763
mxmkiv <57045650+mxmkiv@users.noreply.github.com>
2020-09-01 16:40:23 +0300

    update hello.txt

--- node_js_server/index.js
+++ node_js_server/index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const mysql = require("mysql2");
 const app = express();
 
 const files = ['zero', 'one', 'two']
 
-const urlencodedParser = bodyParser.urlencoded({extended: false});
-console.log(urlencodedParser)
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "frx",
+  password: "frx24"
+});
+
+connection.connect(function(err){
+    if (err) {
+      return console.error("Ошибка: " + err.message);
+    }
+    else{
+      console.log("Подключение к базе успешно установлено");
+    }
+ });
 
 app.get('/', (req,res,next) => {
   res.send("its working");
@@ -15,6 +29,17 @@ app.get('/files', (req,res,next) => {
   console.log('Page', req.query.page);
   res.json({files});
 });
+
+app.get('/registration', (req,res,next) => {
+
+  connection.execute("SELECT * FROM users",
+    function(err, result, fields) {
+      console.log(err);
+      console.log(result);
+      console.log(field);
+    });
+
+  });
 
 app.get('/files/:id', (req, res, next) => {
   if(files[req.params.id]){
